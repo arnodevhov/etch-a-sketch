be810907506fb03a1264ae1a12b5b80082c2eb97
Arno <arn.hovas@gmail.com>
2026-08-01 23:01:34 +0400

    Finish drawing logic implementation

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
 const boardEl = document.querySelector('.board');
 const boardSizeBtn = document.getElementById('set-board-size');
 const randomColorBtn = document.getElementById('random-color');
-const darkenEffectBtn = document.getElementById('darken-effect');
 const colorBtn = document.getElementById('set-color');
+const clearBoardBtn = document.getElementById('clear-board');
+
+let currentColor = colorBtn.value;
+let randomColorActive = false;
+let isDrawing = false;
 
 generateBoard(16);
 
-boardSizeBtn.addEventListener('click', (e) => {
+function generateBoard(size) {
+  boardEl.textContent = '';
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.style.width = 960 / size + 'px';
+    square.style.height = 960 / size + 'px';
+
+    square.addEventListener('mousedown', (e) => {
+      isDrawing = true;
+      draw(e);
+    });
+
+    square.addEventListener('mouseup', () => (isDrawing = false));
+    square.addEventListener('mouseenter', (e) => {
+      if (isDrawing) {
+        draw(e);
+      }
+    });
+
+    boardEl.appendChild(square);
+  }
+}
+
+function draw(e) {
+  if (randomColorActive) {
+    currentColor = setRandomColor();
+  }
+
+  e.target.style.background = currentColor;
+}
+
+function random(number) {
+  return Math.floor(Math.random() * number + 1);
+}
+
+function setRandomColor() {
+  return `rgb(${random(255)} ${random(255)} ${random(255)})`;
+}
+
+boardSizeBtn.addEventListener('click', () => {
   let size = prompt('Enter value from 1 to 100');
+  let sizeNum;
 
   if (size == null) {
     generateBoard(16);
@@ -15,26 +60,29 @@ boardSizeBtn.addEventListener('click', (e) => {
   }
 
   while (true) {
-    if (
-      (size !== null && !Number.isInteger(Number(size))) ||
-      Number(size) <= 0 ||
-      Number(size) > 100
-    ) {
+    sizeNum = Number(size);
+
+    if (!Number.isInteger(sizeNum) || sizeNum <= 0 || sizeNum > 100) {
       size = prompt('Please enter valid number from 1 to 100');
     } else {
       break;
     }
   }
 
-  generateBoard(Number(size));
+  generateBoard(sizeNum);
 });
 
-function generateBoard(size) {
-  boardEl.textContent = '';
-  for (let i = 0; i < size * size; i++) {
-    const square = document.createElement('div');
-    square.style.width = 960 / size + 'px';
-    square.style.height = 960 / size + 'px';
-    boardEl.appendChild(square);
-  }
-}
+boardEl.addEventListener('mouseleave', () => (isDrawing = false));
+
+colorBtn.addEventListener('change', (e) => {
+  randomColorActive = false;
+  currentColor = e.target.value;
+});
+
+clearBoardBtn.addEventListener('click', () => {
+  document.querySelectorAll('.board > div').forEach((square) => {
+    square.style.background = '#fff';
+  });
+});
+
+randomColorBtn.addEventListener('click', () => (randomColorActive = true));
